28f1f8a7160214a604e7e22f86f0f25e0014094e
jude-gabriel <judepgabriel@gmail.com>
2022-02-06 00:04:38 -0800

    AdminTimeSheet
    Admin timesheet now in app

--- time/App.js
+++ time/App.js
@@ -16,6 +16,7 @@ import React from 'react';
 import { UserInterfaceIdiom } from 'expo-constants';
 import Timesheet from './comps/Timesheet';
 import EmployeeHours from './comps/EmployeeHours.js';
+import AdminTimesheet from './comps/admin_timesheet';
 import Employees from './comps/Employees';
 import Card from './comps/Card';
 import TimeCardStart from './comps/TimeCardStart';
@@ -64,7 +65,7 @@ class App extends React.Component {
               // Logged in as admin
               <>
                 <Tab.Screen name="TimeCardStart" component={TimeCardStart}></Tab.Screen>
-                <Tab.Screen name="Timesheet" component={Timesheet}></Tab.Screen>
+                <Tab.Screen name="Timesheet" component={AdminTimesheet}></Tab.Screen>
                 <Tab.Screen name="Employees" component={Employees}></Tab.Screen>
                 <Tab.Screen name="Jobsite" component={Jobsite}></Tab.Screen>
               </>
