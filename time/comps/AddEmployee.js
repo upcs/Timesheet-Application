6fce785f990b33004bf9fb74bfa7e46a807395f7
jude-gabriel <judepgabriel@gmail.com>
2022-02-08 11:36:38 -0800

    Add Employee
    Beginning to implement the functionality of the add employee button.

---
+++ time/comps/AddEmployee.js
@@ -0,0 +1,49 @@
+/*******************************************************************
+ * Functionality for when the 'Add Employee' button is pressed
+ * 
+ * Author: Jude Gabriel 
+ * Date: February 8, 2022
+ ******************************************************************/
+
+import Employees from "./EmployeeInfo";
+
+var firstName;
+var lastName;
+var email;
+var accessType
+
+
+/**
+ * Allows the admin to add an employee
+ */
+class AddEmployee extends React.Component{
+
+
+    //Render the component
+    render(){
+        return (
+            <View>
+                {/* Set Permission type */}
+
+                {/* Set first name  */}
+
+                {/* Set last name  */}
+
+                {/* Set email */}
+            </View> 
+
+        )
+    }
+
+
+
+
+}
+
+
+/**
+ * Styles used for Add Employee component 
+ */
+const styles = StyleSheet.create ({
+
+})
