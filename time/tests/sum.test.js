09c9f0902dfe2dd1e82c6cf4dc80f1b01ea027f6
caden-deutscher <98283923+caden-deutscher@users.noreply.github.com>
2022-02-08 10:50:28 -0800

    First Unit Test

--- time/tests/sum.test.js
+++ time/tests/sum.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EmployeeHours from '../comps/EmployeeHours';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+configure({ adapter: new Adapter() });
 
 describe('First React component test with Enzyme', () => {
    it('renders without crashing', () => {
-      mount(<EmployeeHours/>);
+      shallow(<EmployeeHours/>);
     });
 });
