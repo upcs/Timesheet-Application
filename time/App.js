b15e615604d9969e90c00231d8fca1a25c82f166
haydena23 <haydena23@up.edu>
2022-02-23 21:43:27 -0800

    Added Splash screen functionality

--- time/App.js
+++ time/App.js
@@ -11,6 +11,9 @@ import Login from './comps/login';
 import { Color } from './comps/Palette.js';
 import User from './comps/User';
 
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
 import { UserInterfaceIdiom } from 'expo-constants';
@@ -56,9 +59,53 @@ class App extends React.Component {
 
   }
 
+  /** Leaving this in here for future sprint Splash Screen testing */
+  // splash() {
+  //   const [appIsReady, setAppIsReady] = useState(false);
+  
+  //   useEffect(() => {
+  //     async function prepare() {
+  //       try {
+  //         // Keep the splash screen visible while we fetch resources
+  //         await SplashScreen.preventAutoHideAsync();
+  //         // Pre-load fonts, make any API calls you need to do here
+  //         await Font.loadAsync(Entypo.font);
+  //         // Artificially delay for two seconds to simulate a slow loading
+  //         // experience. Please remove this if you copy and paste the code!
+  //         await new Promise(resolve => setTimeout(resolve, 2000));
+  //       } catch (e) {
+  //         console.warn(e);
+  //       } finally {
+  //         // Tell the application to render
+  //         setAppIsReady(true);
+  //       }
+  //     }
+  //     prepare();
+  //   }, []);
+  
+  //   const onLayoutRootView = useCallback(async () => {
+  //     if (appIsReady) {
+  //       // This tells the splash screen to hide immediately! If we call this after
+  //       // `setAppIsReady`, then we may see a blank screen while the app is
+  //       // loading its initial state and rendering its first pixels. So instead,
+  //       // we hide the splash screen once we know the root view has already
+  //       // performed layout.
+  //       await SplashScreen.hideAsync();
+  //     }
+  //   }, [appIsReady]);
+  
+  //   if (!appIsReady) {
+  //     return null;
+  //   }
+  //   return (
+  //     onLayoutRootView
+  //   );
+  // }
+
   render() {
     const signedIn = this.state.signedIn;
     const isAdmin = this.state.user == User.ADMIN;
+
     return (
       <SafeAreaView style={safeAreaAndroid.SafeArea}>
       <NavigationContainer>
