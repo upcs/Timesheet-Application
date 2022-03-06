b1d03d354cd8ed8169f2624d2ea558213608fd18
Harrison-Winters <83734330+Harrison-Winters@users.noreply.github.com>
2022-03-06 13:21:50 -0800

    Copied over Admin Jobsite from Harrison4

--- time/comps/AdminJobsite.js
+++ time/comps/AdminJobsite.js
@@ -1,3 +1,97 @@
+// /************************************************
+//  * Admin Jobsite Page
+//  * 
+//  * Author: Harrison Winters
+//  * Date: February 5, 2022
+//  ************************************************/
+
+//  import React from 'react';
+//  import {Color} from './Palette.js';
+//  import { Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native'
+//  import { useState } from 'react';
+//  import TimeSheetList from './TimeSheetList.js';
+//  import SearchBar from './search_bar.js';
+//  import AddJobsite from './AddJobsite.js'
+//  import JobsList from './JobsList.js';
+
+
+//  //Jobsite Selection with a search bar and "add jobsite" button
+//  class AdminJobsite extends React.Component {
+     
+//      render() {
+//         let jobData = {};
+       
+//         const addData = (params) => {
+//             jobData = params;
+//             console.log(jobData);
+//         }
+        
+//          return (
+//              <View style={styles.container}>
+//                  <View style={styles.upperbar}>
+//                     <SearchBar style={styles.search}></SearchBar>
+//                     <View style={styles.buttonContainer}>
+//                         <AddJobsite  sendData={addData}></AddJobsite>
+//                     </View>
+                    
+                    
+//                 </View>
+//                 <JobsList></JobsList>
+//              </View>
+//          ) 
+//      }
+//  }
+ 
+//  /*  Styles used for login screen */
+//  const styles = StyleSheet.create({
+//      container: {
+//         //  alignItems: 'center', 
+//         //  justifyContent: 'center',
+//         //  flex: 0.8
+        
+//      },
+
+//      upperbar: {
+        
+//         display: 'flex',
+//         flexDirection: 'row',
+        
+//      },
+
+//     //  logo: { 
+//     //      aspectRatio: 0.9, 
+//     //      resizeMode: 'contain'
+//     //  },
+//      add: {
+//        backgroundColor: Color.MAROON, 
+//        padding: 20, 
+//        marginTop: 12,
+//        borderRadius: 30,
+//        width: 100, 
+//        height: 10,
+//        alignItems: 'center',
+//         position: 'absolute',
+//         justifyContent: 'center',
+//        marginHorizontal: 50,
+       
+    
+//      },
+//      text: {
+//          color: 'white',
+//          fontSize: 14,    
+//         position: 'absolute',
+//         // margin: 'auto',
+//         textAlign: 'center',
+//     },
+
+//     buttonContainer: {
+//        justifyContent: 'center', 
+//        position: 'relative',
+//     }
+//  });
+ 
+//  export default AdminJobsite;
+
 /************************************************
  * Admin Jobsite Page
  * 
@@ -11,45 +105,97 @@
  import { useState } from 'react';
  import TimeSheetList from './TimeSheetList.js';
  import SearchBar from './search_bar.js';
- import AddJobsite from './AddJobsite.js'
+ import AddEmployee from './AddEmployee.js'
  import JobsList from './JobsList.js';
+import FakeEmployeeData from './FakeEmployeeData.js';
+import { FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+import { Button } from 'react-native';
 
 
  //Jobsite Selection with a search bar and "add jobsite" button
  class AdminJobsite extends React.Component {
+
+    constructor(props) {
+      super(props);
+        this.currValue = this.currValue.bind(this, );
+        this.state = {
+          query: ""
+        };
+
+      
+
+        const {employees} = FakeEmployeeData;
+        this.items = FakeEmployeeData;
+        //this.query = "";
+        
+        
+
+        const Item = ({ title }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{title}</Text>
+          </View>
+        );
+
+        this.renderItem = ({ item }) => (
+          <Item title={item.firstName} />
+        );
+        
+      }
+
+      setQuery(newQuery) {
+        return newQuery;
+
+      }
+      
+
+      getFilteredItems(query, items) {
+         if (!query) {
+          return items;
+         }
+        return items.filter((employee) => employee.firstName.includes(query));
+      }
+
+      currValue(newValue) {
+        //console.log(newValue);
+        this.setState({query : newValue});
+       // console.log(this.state.query);
+        this.forceUpdate();
+      }
      
      render() {
-        let jobData = {};
-       
-        const addData = (params) => {
-            jobData = params;
-            console.log(jobData);
-        }
-        
+
+      this.filteredItems = this.getFilteredItems(this.state.query, FakeEmployeeData);
+      
          return (
-             <View style={styles.container}>
+
+
+
+            <View style={styles.container}>
                  <View style={styles.upperbar}>
-                    <SearchBar style={styles.search}></SearchBar>
-                    <View style={styles.buttonContainer}>
-                        <AddJobsite  sendData={addData}></AddJobsite>
+                     <SearchBar style={styles.search} currValue = {this.currValue}></SearchBar>
+                     <View style={styles.buttonContainer}>
+                         <AddEmployee></AddEmployee>
                     </View>
                     
-                    
-                </View>
-                <JobsList></JobsList>
+                 </View>
+                 
+                 <SafeAreaView style={styles.container}>
+                    <FlatList  style= {{backgroundColor: "white"}} renderItem={this.renderItem}  data = {this.filteredItems} ></FlatList>
+                 </SafeAreaView>
+         
+
              </View>
-         ) 
+                  
+         )
+         
      }
  }
  
  /*  Styles used for login screen */
  const styles = StyleSheet.create({
-     container: {
-        //  alignItems: 'center', 
-        //  justifyContent: 'center',
-        //  flex: 0.8
-        
-     },
+     
 
      upperbar: {
         
@@ -58,10 +204,7 @@
         
      },
 
-    //  logo: { 
-    //      aspectRatio: 0.9, 
-    //      resizeMode: 'contain'
-    //  },
+  
      add: {
        backgroundColor: Color.MAROON, 
        padding: 20, 
@@ -77,7 +220,7 @@
     
      },
      text: {
-         color: 'white',
+        
          fontSize: 14,    
         position: 'absolute',
         // margin: 'auto',
--- time/comps/FakeEmployeeData.js
+++ time/comps/FakeEmployeeData.js
@@ -1,15 +1,15 @@
 //in this fake data using 1 for admin 0 for basic
 
 export default FakeData = [
-    {id: 1, firstName: 'Employee', lastName: 'One', userType: 1},
-    {id: 2, firstName: 'Employee', lastName: 'Two', userType: 0},
-    {id: 3, firstName: 'Employee', lastName: 'Three', userType: 1},
-    {id: 4, firstName: 'Employee', lastName: 'Four', userType: 0},
-    {id: 5, firstName: 'Employee', lastName: 'Five', userType: 0},
-    {id: 6, firstName: 'Employee', lastName: 'Six', userType: 1},
-    {id: 7, firstName: 'Employee', lastName: 'Seven', userType: 0},
-    {id: 8, firstName: 'Employee', lastName: 'Eight', userType: 1},
-    {id: 9, firstName: 'Employee', lastName: 'Nine', userType: 0},
-    {id: 10, firstName: 'Employee', lastName: 'Ten', userType: 0},
-    {id: 11, firstName: 'Employee', lastName: 'Eleven', userType: 0},
+    {id: 1, firstName: '1Employee', lastName: 'One', userType: 1},
+    {id: 2, firstName: '2Employee', lastName: 'Two', userType: 0},
+    {id: 3, firstName: '3Employee', lastName: 'Three', userType: 1},
+    {id: 4, firstName: '4Employee', lastName: 'Four', userType: 0},
+    {id: 5, firstName: '5Employee', lastName: 'Five', userType: 0},
+    {id: 6, firstName: '6Employee', lastName: 'Six', userType: 1},
+    {id: 7, firstName: '7Employee', lastName: 'Seven', userType: 0},
+    {id: 8, firstName: '8Employee', lastName: 'Eight', userType: 1},
+    {id: 9, firstName: '9Employee', lastName: 'Nine', userType: 0},
+    {id: 10, firstName: '10Employee', lastName: 'Ten', userType: 0},
+    {id: 11, firstName: '11Employee', lastName: 'Eleven', userType: 0},
 ]
