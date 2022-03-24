273971827ff7d54c9ef83d741179bb329b076bd7
haydena23 <haydena23@up.edu>
2022-03-24 10:53:35 -0700

    Added Crypto package for hashing

--- time/database-communication/database.js
+++ time/database-communication/database.js
@@ -320,7 +320,7 @@ class Database {
 
             clockOutHour: hour, 
             clockOutMinute: minute,
-            totalPunchTimeInMinutes: duration / (1000 * 60),            
+            totalPunchTimeInMinutes: totalTimeInMinutes //duration / (1000 * 60),            
         });   
     }
 
